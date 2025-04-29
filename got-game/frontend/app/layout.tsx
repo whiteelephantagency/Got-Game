5428b699af27bd3340c050875e79fe25dc4762fe
oluwakibati omotosho <omotoshokibati2@gmail.com>
2025-04-29 09:13:40 -0400

    still fixing errors

--- got-game/frontend/app/layout.tsx
+++ got-game/frontend/app/layout.tsx
@@ -3,7 +3,6 @@ import type {Metadata} from "next"
 import "./globals.css"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "../components/theme-provider"
-
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
@@ -17,9 +16,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark">
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
           {children}
         </ThemeProvider>
       </body>
